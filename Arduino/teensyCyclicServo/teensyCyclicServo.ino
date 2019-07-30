990479b9c6788d2fa9cb26461ddd7fa509f837a8
Nick Zhang <zzy@mit.edu>
2019-07-30 16:44:15 -0400

    TeensyDelay test

--- Arduino/teensyCyclicServo/teensyCyclicServo.ino
+++ Arduino/teensyCyclicServo/teensyCyclicServo.ino
@@ -6,6 +6,9 @@
 // actions
 // Uses PDB timer
 //#include <PWMServo.h>
+
+// uses FTM0 timer by default, interferes with PWM function on some pins, see https://www.pjrc.com/teensy/td_pulse.html
+#include <TeensyDelay.h>
 #define NONE 0
 #define LED_OFF 1
 #define LED_ON 2
@@ -22,6 +25,7 @@
 #define PIN_LED4 9
 
 #define PIN_SERVO 4
+#define PIN_TEST 3
 
 
 
@@ -37,8 +41,7 @@
 const float pi = 3.1415926535898;
 
 float ffmod(float x, float y){
-  int quotient = floor(x/y);
-  return x-y*quotient;
+  return x-y*floor(x/y);
 }
 
 float fmap(float x, float in_min, float in_max, float out_min, float out_max) {
@@ -48,7 +51,7 @@ float fmap(float x, float in_min, float in_max, float out_min, float out_max) {
 
 //PWMServo myservo;
 
-IntervalTimer Timer1;
+IntervalTimer blinkerTimer;
 volatile byte pending_action_blinker = NONE;
 volatile bool flash_in_progress = false;
 volatile float on_time_ms = 20;
@@ -61,7 +64,7 @@ void blinker() {
             digitalWrite(PIN_LED2,LOW);
             digitalWrite(PIN_LED3,LOW);
             pending_action_blinker = LED_OFF;
-            Timer1.begin(blinker,on_time_ms*1000);
+            blinkerTimer.begin(blinker,on_time_ms*1000);
             break;
 
         case LED_OFF:
@@ -71,7 +74,7 @@ void blinker() {
             digitalWrite(PIN_LED3,HIGH);
             pending_action_blinker  = NONE;
             flash_in_progress = false;
-            Timer1.end();
+            blinkerTimer.end();
             break;
 
         case NONE:
@@ -81,64 +84,109 @@ void blinker() {
 } 
 
 
-IntervalTimer Timer2;
 // ctrl_phase: phase to START command to max deflection
 volatile float ctrl_phase;
 // 0:azimuth, rad; 1:omega, rad/s
 volatile float state_buffer[2];
 // micros()
-volatile uint16_t state_buffer_ts;
-// ms to next control phase, time between each new ISR
+volatile unsigned long state_buffer_ts;
+// us to next control phase, time between each new ISR
 volatile float quarter_period;
 volatile float current_phase;
 volatile int pending_action_cyclic = NONE;
+// TeensyDelay uses 16bit FTM timer that only goes to about 69ms, if we need a delay longer
+// than that we need to handle this gracefully
+volatile uint32_t remaining_delay_us;
 void cyclic(){
+  if (remaining_delay_us!=0){
+    if (remaining_delay_us > 60000){
+      TeensyDelay::trigger(60000,0);
+      remaining_delay_us -= 60000;
+    } else {
+      TeensyDelay::trigger(remaining_delay_us);
+      remaining_delay_us = 0;
+    }
+    return;
+  }
 
   switch(pending_action_cyclic){
 
       case RISING_NEUTRAL:
           Serial.println("RISING_NEUTRAL");
           setPulsewidth(CENTRAL_SERVO_PULSEWIDTH);
+          digitalWrite(PIN_TEST,LOW);
           pending_action_cyclic = SERVO_MAX;
           // adjust control phase to sync with estimated state
-          // 500 = 2(for 2pi) * 1000 (sec -> ms) / 4 (quarter period)
-          quarter_period = 500.0*pi/state_buffer[1];
+          // 5e5 = 2(for 2pi) * 1e6 (sec -> us) / 4 (quarter period)
+          quarter_period = 5e5*pi/state_buffer[1];
 //XXX would micro() still work here?
-          current_phase = state_buffer[0] + ffmod(micros()-state_buffer_ts,65536)*6.4e-5*state_buffer[1];
-          // make sure next_action_t2 gets a positive delay time
-          //Serial.print("du(ms)=");
-          //Serial.println(quarter_period-ffmod(ctrl_phase-current_phase,2*pi)/state_buffer[1]*1000);
-          // experiment shows roughly 1ms error due to processing time,which results in a ~26ms correction periodically, not sure what to do
-          Timer2.begin(cyclic,(ffmod(ctrl_phase-current_phase,2*pi)/state_buffer[1])*1e6);
+          current_phase = state_buffer[0] + float(micros()-state_buffer_ts)*1e-6*state_buffer[1];
+          remaining_delay_us = (ffmod(ctrl_phase-current_phase,2*pi)/state_buffer[1])*1e6;
+          //Serial.print("current phase= ");
+          //Serial.println(ffmod(current_phase,2*pi)/pi*180);
+          //Serial.print("dist 2 ctrl phase= ");
+          //Serial.println((ffmod(ctrl_phase-current_phase,2*pi))/pi*180);
+          //Serial.print("wakeup after(s) ");
+          //Serial.println((ffmod(ctrl_phase-current_phase,2*pi)/state_buffer[1]));
+          //Serial.println(ffmod((ctrl_phase-current_phase)/2/pi,360));
           break;
           
       case SERVO_MAX:
           Serial.println("SERVO_MAX");
           setPulsewidth(MAX_SERVO_PULSEWIDTH);
+          digitalWrite(PIN_TEST,HIGH);
           pending_action_cyclic = FALLING_NEUTRAL;
-          Timer2.begin(cyclic,quarter_period);
+          remaining_delay_us = quarter_period;
+
+          current_phase = state_buffer[0] + float(micros()-state_buffer_ts)*1e-6*state_buffer[1];
+          //Serial.print("current phase= ");
+          //Serial.println(ffmod(current_phase,2*pi)/pi*180);
+          //Serial.print("dist 2 ctrl phase= ");
+          //Serial.println((ffmod(ctrl_phase-current_phase,2*pi))/pi*180);
           break;
 
       case FALLING_NEUTRAL:
           Serial.println("FALLING_NEUTRAL");
           setPulsewidth(CENTRAL_SERVO_PULSEWIDTH);
+          digitalWrite(PIN_TEST,LOW);
           pending_action_cyclic = SERVO_MIN;
-          Timer2.begin(cyclic,quarter_period);
+          remaining_delay_us = quarter_period;
+
+          current_phase = state_buffer[0] + float(micros()-state_buffer_ts)*1e-6*state_buffer[1];
+          //Serial.print("current phase= ");
+          //Serial.println(ffmod(current_phase,2*pi)/pi*180);
+          //Serial.print("dist 2 ctrl phase= ");
+          //Serial.println((ffmod(ctrl_phase-current_phase,2*pi))/pi*180);
           break;
 
       case SERVO_MIN:
           Serial.println("SERVO_MIN");
           setPulsewidth(MIN_SERVO_PULSEWIDTH);
+          digitalWrite(PIN_TEST,HIGH);
           pending_action_cyclic  = RISING_NEUTRAL;
-          Timer2.begin(cyclic,quarter_period);
+          remaining_delay_us = quarter_period;
+
+          current_phase = state_buffer[0] + float(micros()-state_buffer_ts)*1e-6*state_buffer[1];
+          //Serial.print("current phase= ");
+          //Serial.println(ffmod(current_phase,2*pi)/pi*180);
+          //Serial.print("dist 2 ctrl phase= ");
+          //Serial.println((ffmod(ctrl_phase-current_phase,2*pi))/pi*180);
           break;
 
       case NONE:
       default:
-          Serial.println("fallthrough");
+          //Serial.println("fallthrough");
           break;
   }
-  //Serial.println("done");
+
+  if (remaining_delay_us > 60000){
+    TeensyDelay::trigger(60000,0);
+    remaining_delay_us -= 60000;
+  } else {
+    TeensyDelay::trigger(remaining_delay_us);
+    remaining_delay_us = 0;
+  }
+  return;
 } 
 
 
@@ -159,12 +207,22 @@ void setPulsewidth(float us)
   // restore resolution
 	analogWriteResolution(oldres);
 	interrupts();
-  Serial.print(duty);
 #endif
 }
 
-
-
+volatile bool light_on = false;
+void test(){
+    if (!light_on){
+      digitalWrite(13,HIGH);
+      TeensyDelay::trigger(60e3,1);
+      light_on = true;
+    } else{
+      digitalWrite(13,LOW);
+      TeensyDelay::trigger(20e3,1);
+      light_on = false;
+    }
+}
+      
 
 void setup() {
     // put your setup code here, to run once:
@@ -174,6 +232,9 @@ void setup() {
     pinMode(PIN_LED2,OUTPUT);
     pinMode(PIN_LED3,OUTPUT);
     pinMode(PIN_LED4,OUTPUT);
+    pinMode(PIN_TEST,OUTPUT);
+    pinMode(6,OUTPUT);
+    digitalWrite(6,HIGH);
     digitalWrite(PIN_LED, LOW);
 
     pinMode(PIN_SERVO,OUTPUT);
@@ -185,12 +246,18 @@ void setup() {
     state_buffer[0] = 0;
     // rad/s
     state_buffer[1] = 2*pi;
-    state_buffer_ts = micros();
+    state_buffer_ts = 0;
     //myservo.attach(PIN_SERVO,560,930);
 
     pending_action_cyclic = RISING_NEUTRAL;
-    Timer1.begin(blinker,1e6);
-    Timer2.begin(cyclic,1e6);
+    blinkerTimer.begin(blinker,1e6);
+    TeensyDelay::begin();
+    TeensyDelay::addDelayChannel(cyclic, 0);
+    //TeensyDelay::addDelayChannel(test, 1);
+    TeensyDelay::trigger(10,0);
+    //TeensyDelay::trigger(10,1);
+
+    
 }
 
 
@@ -208,7 +275,6 @@ void loop() {
         Serial.println();
 
         pending_action_blinker = LED_ON;
-        // Setup LED to blink when azimuth = 0
         if (!flash_in_progress){
           // time needed to travel 10 degrees, so that light will be on for 10 deg
           //on_time_ms = 10.0/180.0*pi/x[1][0]*1000.0;
@@ -218,7 +284,7 @@ void loop() {
           if (blink_delay>0){
             flash_in_progress = true;
             pending_action_blinker = LED_ON;
-            Timer1.begin(blinker,blink_delay*1000);
+            blinkerTimer.begin(blinker,blink_delay*1000);
           }
         }
     }
