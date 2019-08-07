2d1608f2e220b6179d6870d29a0b53aa8557dc0b
Nick Zhang <zzy@mit.edu>
2019-08-07 14:14:28 -0400

    clarky analysis

---
+++ xfoilanalysis.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from xfoil import XFoil
+import matplotlib.pyplot as plt
+xf = XFoil()
+
+xf.load("clarky.dat")
+xf.Re = 1e5
+xf.M = 0
+xf.max_iter = 100
+a, cl, cd, cm, cp = xf.aseq(-2,15,0.5)
+plt.plot(a,cl)
+plt.title("alfa vs cl")
+plt.show()
+
+#plt.plot(xf.airfoil.x,xf.airfoil.y)
+#plt.axis('equal')
+#plt.show()
